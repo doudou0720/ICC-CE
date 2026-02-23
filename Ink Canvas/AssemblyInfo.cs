efe3308325e223fa2636996c0cee4393c1182933
CJKmkp <2564608840@qq.com>
2026-02-23 15:45:49 +0800

    add:i18n

--- Ink Canvas/AssemblyInfo.cs
+++ Ink Canvas/AssemblyInfo.cs
@@ -19,14 +19,8 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+// i18n: 默认/回退语言为简体中文，与 Strings.resx 默认文案一致。
+[assembly: System.Resources.NeutralResourcesLanguage("zh-CN", System.Resources.UltimateResourceFallbackLocation.MainAssembly)]
 
 
 [assembly: ThemeInfo(
